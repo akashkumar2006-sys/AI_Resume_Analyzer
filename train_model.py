354528602deb32a911d92a0b6511cf1c3f7e34a2
Akash Kumar Jha <akashkumarrolex2006@gmail.com>
2026-07-29 18:49:04 +0530

    Implement model training and saving functionality
    Added a machine learning pipeline to train a model using skills data and save it to a file.

--- train_model.py
+++ train_model.py
@@ -1,1 +1,53 @@
+import pandas as pd
+import os
+import joblib
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+
+
+# Dataset path
+DATASET_PATH = "dataset/careers.csv"
+
+
+# Load dataset
+data = pd.read_csv(DATASET_PATH)
+
+
+# Input and output
+X = data["Skills"]
+y = data["Career"]
+
+
+# ML Pipeline
+model = Pipeline([
+
+    ("tfidf", TfidfVectorizer()),
+
+    ("classifier", RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    ))
+
+])
+
+
+# Train model
+model.fit(X, y)
+
+
+# Create model folder
+if not os.path.exists("model"):
+    os.makedirs("model")
+
+
+# Save model
+joblib.dump(
+    model,
+    "model/resume_model.pkl"
+)
+
+
+print("✅ Model trained successfully!")
+print("✅ Model saved: model/resume_model.pkl")
